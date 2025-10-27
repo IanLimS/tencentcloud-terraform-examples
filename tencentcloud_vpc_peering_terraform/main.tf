cd7e19ab36324b0116b3840ca15fba85a4fad08e
IanLimsS <limsukyoung1021@gmail.com>
2025-10-27 10:42:33 +0900

    added cos vpc endpoints and modify sg

--- tencentcloud_vpc_peering_terraform/main.tf
+++ tencentcloud_vpc_peering_terraform/main.tf
@@ -2,7 +2,7 @@
 # VPC 
 ##################
 resource "tencentcloud_vpc" "vpc1" {
-  name       = "ianlim-priv-peering-vpc1"
+  name       = "ianlim-peering-vpc1"
   cidr_block = "10.10.0.0/16"
   tags = {
     SA = "ianlim"
@@ -10,7 +10,7 @@ resource "tencentcloud_vpc" "vpc1" {
 }
 
 resource "tencentcloud_subnet" "subnet1" {
-  name              = "ianlim-priv-peering-subnet1"
+  name              = "ianlim-peering-subnet1"
   cidr_block        = "10.10.1.0/24"
   availability_zone = var.zone
   vpc_id            = tencentcloud_vpc.vpc1.id
@@ -20,7 +20,7 @@ resource "tencentcloud_subnet" "subnet1" {
 }
 
 resource "tencentcloud_vpc" "vpc2" {
-  name       = "ianlim-priv-peering-vpc2"
+  name       = "ianlim-peering-vpc2"
   cidr_block = "10.20.0.0/16"
   tags = {
     SA = "ianlim"
@@ -28,7 +28,7 @@ resource "tencentcloud_vpc" "vpc2" {
 }
 
 resource "tencentcloud_subnet" "subnet2" {
-  name              = "ianlim-priv-peering-subnet2"
+  name              = "ianlim-peering-subnet2"
   cidr_block        = "10.20.1.0/24"
   availability_zone = var.zone
   vpc_id            = tencentcloud_vpc.vpc2.id
@@ -49,10 +49,22 @@ resource "tencentcloud_security_group" "sg1" {
   }
   egress {
     cidr_ip     = "0.0.0.0/0"
+    ip_protocol = "tcp"
+    port_range  = "22011"
+    policy      = "accept"
+  }
+  egress {
+    cidr_ip     = "0.0.0.0/0"
+    ip_protocol = "tcp"
+    port_range  = "22003"
+    policy      = "accept"
+  }
+  egress {
+    cidr_ip     = "0.0.0.0/0"
     ip_protocol = "icmp"
     policy      = "accept"
   }
-  name        = "ianlim-priv-peering-sg1"
+  name        = "ianlim-peering-sg1"
   description = "Allow SSH, HTTPS, ICMP"
   tags = {
     SA = "ianlim"
@@ -65,6 +77,24 @@ resource "tencentcloud_security_group_rule" "sg1_ssh" {
   cidr_ip           = "0.0.0.0/0"
   ip_protocol       = "TCP"
   port_range        = "22"
+  policy            = "accept"
+}
+
+resource "tencentcloud_security_group_rule" "sg1_cos_migration_port1" {
+  security_group_id = tencentcloud_security_group.sg1.id
+  type              = "ingress"
+  cidr_ip           = "0.0.0.0/0"
+  ip_protocol       = "TCP"
+  port_range        = "22011"
+  policy            = "accept"
+}
+
+resource "tencentcloud_security_group_rule" "sg1_cos_migration_port2" {
+  security_group_id = tencentcloud_security_group.sg1.id
+  type              = "ingress"
+  cidr_ip           = "0.0.0.0/0"
+  ip_protocol       = "TCP"
+  port_range        = "22003"
   policy            = "accept"
 }
 
@@ -90,6 +120,18 @@ resource "tencentcloud_security_group" "sg2" {
     cidr_ip     = "0.0.0.0/0"
     ip_protocol = "tcp"
     port_range  = "ALL"
+    policy      = "accept"
+  }
+  egress {
+    cidr_ip     = "0.0.0.0/0"
+    ip_protocol = "tcp"
+    port_range  = "22011"
+    policy      = "accept"
+  }
+  egress {
+    cidr_ip     = "0.0.0.0/0"
+    ip_protocol = "tcp"
+    port_range  = "22003"
     policy      = "accept"
   }
   egress {
@@ -122,6 +164,24 @@ resource "tencentcloud_security_group_rule" "sg2_https" {
   policy            = "accept"
 }
 
+resource "tencentcloud_security_group_rule" "sg2_cos_migration_port2" {
+  security_group_id = tencentcloud_security_group.sg2.id
+  type              = "ingress"
+  cidr_ip           = "0.0.0.0/0"
+  ip_protocol       = "TCP"
+  port_range        = "22011"
+  policy            = "accept"
+}
+
+resource "tencentcloud_security_group_rule" "sg2_cos_migration_port2" {
+  security_group_id = tencentcloud_security_group.sg2.id
+  type              = "ingress"
+  cidr_ip           = "0.0.0.0/0"
+  ip_protocol       = "TCP"
+  port_range        = "22003"
+  policy            = "accept"
+}
+
 resource "tencentcloud_security_group_rule" "sg2_icmp" {
   security_group_id = tencentcloud_security_group.sg2.id
   type              = "ingress"
@@ -134,41 +194,52 @@ resource "tencentcloud_security_group_rule" "sg2_icmp" {
 # CVM
 ##################
 resource "tencentcloud_instance" "cvm1" {
-  instance_name              = "ianlim-priv-peering-cvm1"
+  instance_name              = "ianlim-peering-cvm1"
   image_id                   = var.image_id
   instance_type              = var.instance_type
   subnet_id                  = tencentcloud_subnet.subnet1.id
-  availability_zone          = var.zone
-  internet_max_bandwidth_out = 0
-  orderly_security_groups    = [tencentcloud_security_group.sg1.id]
+}
+
+##################
+# COS Buckets
+##################
+resource "tencentcloud_cos_bucket" "cos1" {
+  bucket = "ianlim-peering-cos1"
+  acl    = "private"
+  region = "ap-seoul"
   tags = {
     SA = "ianlim"
   }
 }
 
-resource "tencentcloud_instance" "cvm2" {
-  instance_name              = "ianlim-priv-peering-cvm2"
-  image_id                   = var.image_id
-  instance_type              = var.instance_type
-  subnet_id                  = tencentcloud_subnet.subnet2.id
-  availability_zone          = var.zone
-  internet_max_bandwidth_out = 0
-  orderly_security_groups    = [tencentcloud_security_group.sg2.id]
+resource "tencentcloud_cos_bucket" "cos2" {
+  bucket = "ianlim-peering-cos2"
+  acl    = "private"
+  region = "ap-seoul"
   tags = {
     SA = "ianlim"
   }
 }
 
 ##################
-# VPC Peering
+# VPC Endpoints
 ##################
+resource "tencentcloud_vpc_endpoint" "endpoint1" {
+  vpc_id          = tencentcloud_vpc.vpc1.id
+  service_id      = "vpcsvc-ocuykgps"
+  peer_account    = "2832742109"
+  peer_account_type = "other tencent account"
+  tags = {
+    SA = "ianlim"
+  }
+}
 
-resource "tencentcloud_vpc_peer_connect_manager" "peering_connection" {
-  source_vpc_id         = tencentcloud_vpc.vpc1.id
-  destination_vpc_id    = tencentcloud_vpc.vpc2.id
-  destination_region    = var.region
-  destination_uin       = var.uin
-  peering_connection_name = "ianlim-priv-peering-peering-connection"
-  # Optional: Set bandwidth, charge type, etc.
-  # bandwidth = 100
+resource "tencentcloud_vpc_endpoint" "endpoint2" {
+  vpc_id          = tencentcloud_vpc.vpc2.id
+  service_id      = "vpcsvc-ocuykgps"
+  peer_account    = "2832742109"
+  peer_account_type = "other tencent account"
+  tags = {
+    SA = "ianlim"
+  }
 }
