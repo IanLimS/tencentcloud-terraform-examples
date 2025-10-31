43175ea8be1d1045bf8597ea8ac89d63f6fc8948
IanLimsS <limsukyoung1021@gmail.com>
2025-10-31 12:47:43 +0900

    db update

--- tencentcloud_database_mysql_terraform/main.tf
+++ tencentcloud_database_mysql_terraform/main.tf
@@ -3,61 +3,45 @@ provider "tencentcloud" {
 }
 
 resource "tencentcloud_vpc" "vpc" {
-  name       = "vpc"
+  name       = "ianlim_db_vpc"
   cidr_block = "10.0.0.0/16"
 }
 
 resource "tencentcloud_subnet" "subnet" {
-  name              = "subnet"
+  name              = "ianlim_db_subnet"
   vpc_id            = tencentcloud_vpc.vpc.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "ap-seoul-1"
 }
 
+
 resource "tencentcloud_security_group" "sg" {
-  name        = "sg"
-  description = "Allow SSH, HTTP, HTTPS, and MySQL"
-  vpc_id      = tencentcloud_vpc.vpc.id
+  name = "ianlim_db_sg"
 }
 
-resource "tencentcloud_security_group_rule" "ssh" {
+resource "tencentcloud_security_group_lite_rule" "sg" {
   security_group_id = tencentcloud_security_group.sg.id
-  type              = "ingress"
-  cidr_ip           = "0.0.0.0/0"
-  ip_protocol       = "TCP"
-  port_range        = "22"
-  policy            = "accept"
+
+  ingress = [
+    "ACCEPT#0.0.0.0/0#22#TCP",
+    "ACCEPT#0.0.0.0/0#80#TCP",
+    "ACCEPT#0.0.0.0/0#443#TCP",
+    "ACCEPT#0.0.0.0/0#3306#TCP",
+    "ACCEPT#0.0.0.0/0#ALL#ICMP",
+  ]
+
+  egress = [
+    "ACCEPT#0.0.0.0/0#22#TCP",
+    "ACCEPT#0.0.0.0/0#80#TCP",
+    "ACCEPT#0.0.0.0/0#443#TCP",
+    "ACCEPT#0.0.0.0/0#3306#TCP",
+    "ACCEPT#0.0.0.0/0#ALL#ICMP",
+  ]
 }
 
-resource "tencentcloud_security_group_rule" "http" {
-  security_group_id = tencentcloud_security_group.sg.id
-  type              = "ingress"
-  cidr_ip           = "0.0.0.0/0"
-  ip_protocol       = "TCP"
-  port_range        = "80"
-  policy            = "accept"
-}
-
-resource "tencentcloud_security_group_rule" "https" {
-  security_group_id = tencentcloud_security_group.sg.id
-  type              = "ingress"
-  cidr_ip           = "0.0.0.0/0"
-  ip_protocol       = "TCP"
-  port_range        = "443"
-  policy            = "accept"
-}
-
-resource "tencentcloud_security_group_rule" "mysql" {
-  security_group_id = tencentcloud_security_group.sg.id
-  type              = "ingress"
-  cidr_ip           = "0.0.0.0/0"
-  ip_protocol       = "TCP"
-  port_range        = "3306"
-  policy            = "accept"
-}
 
 resource "tencentcloud_mysql_instance" "mysql" {
-  instance_name      = "mysql-instance"
+  instance_name      = "ianlim_db_mysql-instance"
   mem_size           = 8000
   volume_size        = 100
   engine_version     = "8.0"
@@ -71,16 +55,20 @@ resource "tencentcloud_mysql_instance" "mysql" {
   internet_port      = 3306
 }
 
-resource "tencentcloud_instance" "cvm" {
-  instance_name      = "cvm-instance"
-  availability_zone  = "ap-seoul-1"
-  image_id           = "img-pi0ii46r"
-  instance_type      = "S5.MEDIUM4"
-  system_disk_type   = "CLOUD_PREMIUM"
-  system_disk_size   = 50
-  vpc_id             = tencentcloud_vpc.vpc.id
-  subnet_id          = tencentcloud_subnet.subnet.id
-  security_groups    = [tencentcloud_security_group.sg.id]
-  internet_max_bandwidth_out = 10
-  allocate_public_ip = true
+resource "tencentcloud_instance" "jump_server" {
+  instance_name       = "ianlim_db_jump_server"
+  instance_type       = "SA5.MEDIUM4"
+  image_id            = "img-9qabwvbn"
+  vpc_id              = tencentcloud_vpc.vpc.id
+  subnet_id           = tencentcloud_subnet.subnet.id
+  security_groups     = [tencentcloud_security_group.sg.id]
+  allocate_public_ip  = true
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y mysql
+              systemctl enable sshd
+              systemctl start sshd
+              EOF
 }
